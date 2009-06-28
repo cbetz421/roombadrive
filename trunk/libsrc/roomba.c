74018fbb725fca18f2d91e12b6ec09882798d39a
woobster <woobster@6ec530da-61ac-11de-ac86-572c61aa6b8a>
2009-06-28 16:28:41 +0000

    initial import

---
+++ trunk/libsrc/roomba.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <strings.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include "roomba.h"
+
+/* Open the serial port, set the params, etc */
+
+/*
+ * 
+ * 
+ * speed 57600 baud; line = 0;
+intr = <undef>; quit = <undef>; erase = <undef>; kill = <undef>; eof = <undef>; start = <undef>; stop = <undef>; susp = <undef>;
+rprnt = <undef>; werase = <undef>; lnext = <undef>; flush = <undef>; min = 0; time = 0;
+-brkint -icrnl -imaxbel
+-opost -onlcr
+-isig -icanon -iexten -echo -echoe -echok -echoctl -echoke
+
+*/
+
+#define SPEED B57600
+//#define SPEED B9600
+
+int serial_open(char *device) {
+	int fd;
+	static struct termios newtio;
+	int status;
+	
+	fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
+	if (fd < 0) {
+		return -1;
+	}
+
+	tcgetattr(fd, &newtio);
+	
+
+
+	newtio.c_cflag = SPEED | CS8 | PARENB | CLOCAL | CREAD;
+
+	cfsetispeed(&newtio, SPEED);
+	cfsetospeed(&newtio, SPEED);
+
+
+	/*
+	IGNPAR : ignore bytes with parity errors
+	otherwise make device raw (no other input processing)
+	*/
+	newtio.c_iflag = IGNPAR;
+
+	/*
+	Raw output.
+	*/
+	newtio.c_oflag = 0;
+
+	/*
+	set input mode (non-canonical, no echo ... )
+	*/
+
+	newtio.c_lflag = 0;
+
+	newtio.c_cc[VTIME] = 0; /* inter-character timer unused */
+	newtio.c_cc[VMIN] = 0;  /* no blocking */
+
+	newtio.c_cflag &= ~PARENB;
+
+	tcflush(fd, TCIFLUSH);
+	if(tcsetattr(fd,TCSANOW,&newtio) < 0) {
+		exit(1);
+		return -1;	
+	}
+
+	/* make sure DTR & RTS outputs are ON */    
+	ioctl(fd, TIOCMGET, &status);
+	status |= (TIOCM_DTR | TIOCM_RTS);
+	ioctl(fd, TIOCMSET, &status);
+	
+	usleep(20000);
+	return fd;	
+}
+
+int serial_close(int fd) {
+	return close(fd);	
+}
+
+
+int serial_write(int fd, char *data, int len) {
+	int i;
+	int res;
+	fprintf(stderr, "TX {%d} => ", len);
+	for(i=0;i<len;i++) {
+		fprintf(stderr, "[%02x:%03d] ", (unsigned char)data[i], (unsigned char)data[i]);	
+	}
+	res = write(fd, data, len);
+	fprintf(stderr, "= {%d}\n", res);
+	return res;
+}
+
+int roomba_wheels(int fd, short left, short right) {
+	char data[5];
+	data[0] = 145;
+	data[1] = ((char *)&left)[1];
+	data[2] = ((char *)&left)[0];
+	data[3] = ((char *)&right)[1];
+	data[4] = ((char *)&right)[0];
+	return serial_write(fd, data, 5);
+}
+
+int roomba_stop(int fd) {
+	return roomba_wheels(fd, 0, 0);
+}
+
+int roomba_start_communication(int fd) {
+	char data[1];
+	int res;
+	data[0] = 128;
+	res = serial_write(fd, data, 1);
+	usleep(20000);
+	return res;
+}
+
+int roomba_init_safe(int fd) {
+	char data[1];
+	int res;
+	data[0] = 131;
+	res = serial_write(fd, data, 1);
+	usleep(20000);
+	return res;
+}
+
+
+_sensor_data* roomba_get_sensor_data(int fd, _sensor_data *sensor_data) {
+	unsigned char data[2];
+	int res;
+	data[0] = 142;
+	data[1] = 6; /*This is all packets*/
+	res = serial_write(fd, (char *)data, 2);
+	usleep(40000);
+	res = read(fd, sensor_data, 52);
+	if(res == 52) {
+		sensor_data->distance = htons(sensor_data->distance);
+		sensor_data->angle = htons(sensor_data->angle);
+		sensor_data->battery_charge = htons(sensor_data->battery_charge);
+
+		sensor_data->battery_capacity = htons(sensor_data->battery_capacity);
+		sensor_data->wall_signal = htons(sensor_data->wall_signal);
+		sensor_data->cliff_left_signal = htons(sensor_data->cliff_left_signal);
+		sensor_data->cliff_front_left_signal = htons(sensor_data->cliff_front_left_signal);
+		sensor_data->cliff_font_right_signal = htons(sensor_data->cliff_font_right_signal);
+		sensor_data->cliff_right_signal = htons(sensor_data->cliff_right_signal);
+		sensor_data->user_analog_input = htons(sensor_data->user_analog_input);
+		sensor_data->velocity = htons(sensor_data->velocity);
+		sensor_data->radius = htons(sensor_data->radius);
+		sensor_data->right_velocity = htons(sensor_data->right_velocity);
+		sensor_data->left_velocity = htons(sensor_data->left_velocity);
+		
+		return sensor_data;
+	}
+	
+	return NULL;
+}
+
